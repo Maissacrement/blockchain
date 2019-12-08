fbbfa15e547c415f7235dc25710593c0cf7ac96b
mario <emmariodelar@gmail.com>
2019-12-08 20:16:48 +0100

    Revert "Implement Blockchain history principle with python"
    This reverts commit d6d1abb08c448b2a96310dfb55b3bd18627c6b0e.

--- Hands-On-Blockchain/Obj/Block.py
+++
@@ -1,5 +0,0 @@
-class Block():
-  def __init__(self, id=None, history=None, parent_id=None):
-    self.id = id
-    self.history = history
-    self.parent_id = parent_id
--- README.md
+++ README.md
@@ -1,12 +1,1 @@
 # blockchain
-
-# Requirement
-
-pip install virtualenv
-
-# Start
-
-Run env:
-
-    `python3 -m venv blockchain`
-    `source blockchain/bin/activate`
